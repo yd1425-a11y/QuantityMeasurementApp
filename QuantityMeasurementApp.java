e7447664208c09fc318a9e1c2d9455be99d9083c
sumasreekannamreddy-dev <sumasreekannamreddy@gmail.com>
2026-04-24 14:35:04 +0530

    added dev branch

---
+++ QuantityMeasurementApp.java
@@ -0,0 +1,27 @@
+public class QuantityMeasurementApp {
+
+    // Inner class
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;              // same reference
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        System.out.println("Equal: " + f1.equals(f2));
+    }
+}
